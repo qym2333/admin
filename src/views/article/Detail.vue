aacc3d74e9b755b4a3dd23b89d2fa3f9c75de8c1
even <731788284@qq.com>
2021-01-30 08:55:39 +0800

    新建文章预览

---
+++ src/views/article/Detail.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+
+  created () {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped lang='less'>
+</style>
